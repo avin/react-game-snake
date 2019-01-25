a9de984526e961172fd30773f79d40b42187bd5b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-25 19:30:22 +0000

    Deploy avin/react-game-snake to github.com/avin/react-game-snake.git:gh-pages

---
+++ precache-manifest.f38284e4119432025cd8d4cffe95ff0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "17c83d084fafb3d6239f",
+    "url": "./static/css/main.dac69997.chunk.css"
+  },
+  {
+    "revision": "17c83d084fafb3d6239f",
+    "url": "./static/js/main.17c83d08.chunk.js"
+  },
+  {
+    "revision": "37bc46206f5a95eb852f",
+    "url": "./static/css/1.0af516fa.chunk.css"
+  },
+  {
+    "revision": "37bc46206f5a95eb852f",
+    "url": "./static/js/1.37bc4620.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a2d96cb9ec2ada021843e4a23ada1d5d",
+    "url": "./index.html"
+  }
+];
